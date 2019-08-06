163ceef276e4d7f74de7e3445c9f0cf2337ad4b3
CoGrammar Code Review <reviews@cogrammar.com>
2019-08-06 13:29:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,6 @@ def add_element_to_start_of_array(array, element)
   array = ["JP", 23, "Manchester United"]
 
   array.unshift("Won all pre seasons!")
-  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
