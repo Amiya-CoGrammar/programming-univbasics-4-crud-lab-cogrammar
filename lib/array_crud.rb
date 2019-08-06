f2d819dc29aea98ed7d9b18d03bb3988529a9954
CoGrammar Code Review <reviews@cogrammar.com>
2019-08-06 13:35:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 add_element_to_start_of_array(["JP", 23, "Manchester United"], "Won all pre seasons!")
 
 def remove_element_from_end_of_array(array)
-
+  array.pop(element)
 end
 
 def remove_element_from_start_of_array(array)
