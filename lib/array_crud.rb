4a82be0907474ce9e0ff41e43cb2368015aba4cd
CoGrammar Code Review <reviews@cogrammar.com>
2019-08-06 13:33:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array << element
+  array.push(element) 
 end
 add_element_to_end_of_array(["JP", 23, "Manchester United"], "Won all pre seasons!")
 
